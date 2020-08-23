7068cf7f91214e258f2a71b672afdb16dae96b9e
Ngo Dang tan <ngodangtan1994@gmail.com>
2020-08-23 23:35:33 +0700

    done ui contact

--- components/View/ContactView.js
+++ components/View/ContactView.js
@@ -1,10 +1,83 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
-export default class ContactView extends Component{
-    render(){
-        return(
-            <View></View>
+import {
+    View,
+    TextInput,
+    TouchableOpacity,
+    Text
+} from 'react-native';
+import { contactViewStyles } from '../styles/styles';
+import { ScrollView } from 'react-native-gesture-handler';
+export default class ContactView extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { HoTen: '', Phone: '', Email: '', Note: '' }
+    }
+    render() {
+        return (
+            <View style={contactViewStyles.container}>
+                <ScrollView>
+                    <View style={{ flex: 2, justifyContent: 'flex-start' }}>
+                        <Text style={contactViewStyles.text_title}>
+                            Họ tên
+                    </Text>
+                        <TextInput onChangeText={(HoTen) => this.setState({ HoTen })}
+                            value={this.state.HoTen} placeholder="Vui lòng nhập họ tên" style={contactViewStyles.input} />
+
+                        <Text style={contactViewStyles.text_title}>
+                            Số điện thoại
+                    </Text>
+                        <TextInput onChangeText={(Phone) => this.setState({ Phone })}
+                            value={this.state.Phone} placeholder="Vui lòng nhập số điện thoại" style={contactViewStyles.input} />
+
+                        <Text style={contactViewStyles.text_title}>
+                            Email
+                    </Text>
+                        <TextInput onChangeText={(Email) => this.setState({ Email })}
+                            value={this.state.Email} placeholder="Vui lòng nhập email" style={contactViewStyles.input} />
+
+                        <Text style={contactViewStyles.text_title}>
+                            Nội dung cần liên hệ
+                    </Text>
+                        <TextInput onChangeText={(Note) => this.setState({ Note })}
+                            value={this.state.Note} placeholder="Vui lòng nhập nội dung cần liên hệ" style={contactViewStyles.input_detail} />
+                        <TouchableOpacity onPress={this.XLNhan.bind(this)} activeOpacity={0.5}>
+                            <View style={contactViewStyles.button}>
+                                <Text style={contactViewStyles.text}>
+                                    Liên Hệ
+                        </Text>
+                            </View>
+
+
+                        </TouchableOpacity>
+
+                    </View>
+
+
+                </ScrollView>
+
+
+            </View>
         )
+    }
+    XLNhan() {
+        if (this.state.HoTen == "") {
+            alert('Vui lòng nhập họ tên !')
+            return
+        }
+        if (this.state.Phone == "") {
+            alert('Vui lòng nhập số điện thoại !')
+            return
+
+        }
+        if (this.state.Email == "") {
+            alert('Vui lòng nhập Email !')
+            return
+        }
+        if (this.state.Note == "") {
+            alert('Vui lòng nhập nội dung liên hệ')
+            return
+        }
+
     }
 }
---
+++ components/styles/styles.js
@@ -0,0 +1,56 @@
+
+import { Dimensions, StyleSheet } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+export const contactViewStyles = StyleSheet.create({
+ 
+    container:{
+        flex:1,
+        backgroundColor:'#0082c8',
+        justifyContent:'center'
+    },
+    input:{
+        height:44,
+        paddingHorizontal:6,
+        backgroundColor:Colors.white,
+        margin: 5,
+        borderRadius:5,
+        marginLeft:10,
+        marginRight:10,
+    
+    },
+    input_detail:{
+        height:100,
+        paddingHorizontal:6,
+        backgroundColor:Colors.white,
+        margin: 5,
+        borderRadius:5,
+        marginLeft:10,
+        marginRight:10
+    },
+    text_title:{
+        color:Colors.white,
+        fontSize:16,
+        marginLeft:10,
+        marginRight:10,
+        marginTop:10
+    },
+    text:{
+        color:Colors.white,
+        fontSize:16
+    },
+    button:{
+        height:46,
+        borderRadius:5,
+        margin:5,
+        backgroundColor:'#036fa9',
+        justifyContent:'center',
+        alignItems:'center',
+        marginTop:20,
+        marginLeft:10,
+        marginRight:10
+    }
+ 
+ 
+
+
+});
